6d8672b9ced02425b13c5585ead5707ab8781565
ibbrun <carlo.brun@stud.hslu.ch>
2020-05-05 15:16:27 +0200

    Shiny App erste Versuche, erstellen eines reaktiven histogramms

--- dasb_project/shiny_app.R
+++ dasb_project/shiny_app.R
@@ -1,7 +1,64 @@
 # shinyapp template
 library(shiny)
-ui <- fluidPage()
+library(ggplot2)
+library(dplyr)
+df <- read.csv("student_merged.csv", stringsAsFactors = FALSE)
 
-server <- function(input, output) {}
+
+ui <- fluidPage(
+  titlePanel("Student Alcohol Consumption"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      
+      titlePanel("Exploratory Data Analysis"),
+      
+      sliderInput("ageInput", "Age", min = 15, max = 22,
+                             value = c(18, 20)),
+      radioButtons("subjectInput", "School subject",
+                   choices = c("MS", "GP", "Both"),
+                   selected = "MS"),
+      radioButtons("genderInput", "Country",
+                  choices = c("M", "F", "Both"),
+                  selected = "F"),
+    ),
+  
+    mainPanel(
+      plotOutput("coolplot"),
+      br(), br(),
+      tableOutput("results")
+      
+    )
+  )
+)
+
+
+
+
+server <- function(input, output) {
+  output$coolplot <- renderPlot({
+    filtered <-
+      df %>%
+      filter(age >= input$ageInput[1],
+             age <= input$ageInput[2],
+             school == input$subjectInput,
+             sex == input$genderInput
+      )
+    ggplot(filtered, aes(failures)) +
+      geom_histogram()
+  })
+  
+  output$results <- renderTable({
+    filtered <-
+      df %>%
+      filter(age >= input$ageInput[1],
+             age <= input$ageInput[2],
+             school == input$subjectInput,
+             sex == input$genderInput
+      )
+    filtered
+  })
+  
+}
 
 shinyApp(ui = ui, server = server)
