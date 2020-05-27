5b2e43a1fb545a5465b05318be616e699a66ca60
ibbrun <carlo.brun@stud.hslu.ch>
2020-05-27 15:00:34 +0200

    various changes

--- dasb_project/shiny_app.R
+++ dasb_project/shiny_app.R
@@ -22,8 +22,44 @@ ui <- fluidPage(theme = shinytheme("flatly"),
                         uiOutput("pdfview")
                ),
              
-               tabPanel("Information about the Data", "Tbd"
-                        ),
+               tabPanel("Information about Data", mainPanel(
+                        HTML("<h2>Data insights</h2>
+<h4>The following list will help you understand all variables of the dataset. This will provide you with everything you need to build an exploratory data analysis by yourself.</h4>
+<br></br>
+<ul>
+<li>school - student's school (binary: 'GP' - Gabriel Pereira or 'MS' - Mousinho da Silveira)</li>
+<li>sex - student's sex (binary: 'F' - female or 'M' - male) </li>
+<li>age - student's age (numeric: from 15 to 22) </li>
+<li>address - student's home address type (binary: 'U' - urban or 'R' - rural) </li>
+<li>famsize - family size (binary: 'LE3' - less or equal to 3 or 'GT3' - greater than 3) </li>
+<li>Pstatus - parent's cohabitation status (binary: 'T' - living together or 'A' - apart)</li>
+<li>Medu - mother's education (numeric: 0 - none,  1 - primary education (4th grade), 2 – 5th to 9th grade, 3 – secondary education or 4 – higher education)</li>
+<li>Fedu - father's education (numeric: 0 - none,  1 - primary education (4th grade), 2 – 5th to 9th grade, 3 – secondary education or 4 – higher education)</li>
+<li>Mjob - mother's job (nominal: 'teacher', 'health' care related, civil 'services' (e.g. administrative or police), 'at_home' or 'other') </li>
+<li>Fjob - father's job (nominal: 'teacher', 'health' care related, civil 'services' (e.g. administrative or police), 'at_home' or 'other') </li>
+<li>reason - reason to choose this school (nominal: close to 'home', school 'reputation', 'course' preference or 'other') </li>
+<li>guardian - student's guardian (nominal: 'mother', 'father' or 'other') </li>
+<li>traveltime - home to school travel time (numeric: 1 - 1 hour) </li>
+<li>studytime - weekly study time (numeric: 1 - 10 hours) </li>
+<li>failures - number of past class failures (numeric: n if 1&lt;=n&lt;3, else 4) </li>
+<li>schoolsup - extra educational support (binary: yes or no) </li>
+<li>famsup - family educational support (binary: yes or no) </li>
+<li>paid - extra paid classes within the course subject (Math or Portuguese) (binary: yes or no) </li>
+<li>activities - extra-curricular activities (binary: yes or no) </li>
+<li>nursery - attended nursery school (binary: yes or no) </li>
+<li>higher - wants to take higher education (binary: yes or no) </li>
+<li>internet - Internet access at home (binary: yes or no) </li>
+<li>romantic - with a romantic relationship (binary: yes or no) </li>
+<li>famrel - quality of family relationships (numeric: from 1 - very bad to 5 - excellent) </li>
+<li>freetime - free time after school (numeric: from 1 - very low to 5 - very high) </li>
+<li>goout - going out with friends (numeric: from 1 - very low to 5 - very high) </li>
+<li>Dalc - workday alcohol consumption (numeric: from 1 - very low to 5 - very high) </li>
+<li>Walc - weekend alcohol consumption (numeric: from 1 - very low to 5 - very high) </li>
+<li>health - current health status (numeric: from 1 - very bad to 5 - very good) </li>
+<li>absences - number of school absences (numeric: from 0 to 93) </li>
+</ul>")
+                        )
+               ),
                
                tabPanel("Exploratory Data Analysis",
                         sidebarLayout(
